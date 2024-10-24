ee79e622b20fbf25082a2513d1ac8e43b2c9c16e
OskarJaszczor <oskarjaszczor@gmail.com>
2024-10-24 08:50:21 +0200

    tets
    test

--- projekt/src/App.jsx
+++ projekt/src/App.jsx
@@ -29,6 +29,7 @@ function App() {
     <div className='mainContainer'>
       <div className='topBar'>
         <span>Connectify</span>
+        <h1>chujchuj123123</h1>
       </div>
     </div>
   )
