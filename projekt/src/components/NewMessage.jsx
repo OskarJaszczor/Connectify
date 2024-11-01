d34b5c099a0421dfd674a197038743e4061e835f
OskarJaszczor <oskarjaszczor@gmail.com>
2024-11-01 22:25:36 +0100

    new
    chuja zmienilem

--- projekt/src/components/NewMessage.jsx
+++ projekt/src/components/NewMessage.jsx
@@ -1,51 +1,60 @@
-/* eslint-disable react/prop-types */
 import { useState } from 'react'
 
-export default function NewMessage({activeChannel}) {
-    const [message, newMessage] = useState('')
+export default function NewMessage({ activeChannel }) {
+	const [message, newMessage] = useState('')
 
-    const handleSendMessage = async() => {
-        if (message.trim() === '') return
-        console.log(message)
-        try {
-            const response = await fetch('http://localhost:3000/messages', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    content: message,
-                    author: 'User1',
-                    channel: activeChannel,
-                    date: new Date().toLocaleDateString(),
-                    hour: new Date().toLocaleTimeString(),
-                }),
-            })
+	const handleInputChange = event => {
+		newMessage(event.target.value)
+	}
 
-            const result = JSON.parse(response)
-            if (result.success) {
-                console.log('Wiadomość: ' + message + ' wysłana.')
-                newMessage('')
-            }
-        } catch (error) {
-            console.log('Nie udało się wysłać wiadomości!', error)
-        }
-    }
+	const handleSendMessage = async () => {
+		if (message.trim() === '') return
+		console.log('Wiadomość do wysłania:', message)
+		try {
+			console.log('Wiadomość do wysłania juz dalej:', message)
 
-    return ( 
-        <div className='newMessageContainer'>
-        <input type = "text" value = { message }
-        onChange = { msg => newMessage(msg.target.value) }
-        placeholder = "Type here..."
-        name = ""
-        className = "newMessageInput" />
-        <button onClick = { handleSendMessage }
-        type = "submit"
-        style = {
-            { backgroundImage: `url(./img/send.png)`, color: `red` } } className = "newMessageSend">submit </button> 
-        </div>
-    )
+			const response = await fetch('http://localhost:3000/messages', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({
+					content: message,
+					author: 'User1',
+					channel: activeChannel,
+					date: new Date().toLocaleDateString(),
+					hour: new Date().toLocaleTimeString(),
+				}),
+			})
+
+			const result = await response.json()
+			if (result.success) {
+				console.log('Wiadomość wysłana. Czyszczenie pola.')
+				newMessage('')
+			} else {
+				newMessage('')
+			}
+		} catch (error) {
+			console.log('Nie udało się wysłać wiadomości!', error)
+		}
+	}
+
+	return (
+		<div className="newMessageContainer">
+			<input
+				type="text"
+				value={message}
+				onChange={handleInputChange}
+				placeholder="Type here..."
+				className="newMessageInput"
+			/>
+			<button
+				onClick={handleSendMessage}
+				type="submit"
+				style={{ backgroundImage: `url(./img/send.png)`, color: `red` }}
+				className="newMessageSend">
+				submit
+			</button>
+		</div>
+	)
 }
-
-
-
