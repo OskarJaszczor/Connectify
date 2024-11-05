90c6af8c57f25056cd50681aa05a911b735f73ba
OskarJaszczor <oskarjaszczor@gmail.com>
2024-11-05 21:23:21 +0100

    dziala logowanie i rejestracja
    123321

--- logowanie/script.js
+++ logowanie/script.js
@@ -1,19 +1,78 @@
-document.querySelector(".logInButton").addEventListener("click", () => {
-  document.querySelector(".buttonContainer").innerHTML = "";
-  document.querySelector(".buttonContainer").innerHTML =
-    "<div class='loginContainer'>Login: <br><input type='text' class='inpLogin' name='inpLogin'><br>Hasło:<br> <input type='password' class='inpPassword' name='inpPassword'><br><button class='sendButton'>Zaloguj</button></div>";
-});
+document.querySelector('.logInButton').addEventListener('click', () => {
+	document.querySelector('.buttonContainer').innerHTML = `
+        <div class='loginContainer'>
+            Login: <br><input type='text' class='inpLogin' name='inpLogin' id='login' required><br>
+            Hasło:<br><input type='password' class='inpPassword' name='inpPassword' id='password' required><br>
+            <button class='sendButton'>Zaloguj</button>
+        </div>
+    `
 
-document.querySelector(".signUpButton").addEventListener("click", () => {
-  document.querySelector(".buttonContainer").innerHTML = "";
-  document.querySelector(".buttonContainer").innerHTML =
-    "<div class='registerContainer'>Login: <br><input type='text' class='inpLogin' name='inpLogin'><br>Hasło:<br> <input type='password' class='inpPassword' name='inpPassword'> <br>Potwierdź hasło:<br> <input type='password' class='inpPassword' name='inpPassword2'><button class='sendButton1'>Zarejestruj się</button></div>";
-});
+	const sendButton = document.querySelector('.sendButton')
+	sendButton.addEventListener('click', async () => {
+		const login = document.getElementById('login').value
+		const password = document.getElementById('password').value
 
-// dodawanie i sprawdzanie z userami w bazie
+		try {
+			const response = await fetch('http://localhost:3002/login', {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify({ login, password }),
+			})
 
-// logowanie
-document.querySelector(".sendButton").addEventListener("click", () => {});
+			if (response.ok) {
+				window.location.href = 'http://localhost:5173/'
+			} else {
+				const data = await response.json()
+				alert(data.message || 'Wystąpił błąd podczas logowania')
+			}
+		} catch (error) {
+			console.error('Błąd:', error)
+			alert('Wystąpił błąd podczas logowania')
+		}
+	})
+})
 
-// rejestracja
-document.querySelector(".sendButton1").addEventListener("click", () => {});
+// Rejestracja
+document.querySelector('.signUpButton').addEventListener('click', () => {
+	document.querySelector('.buttonContainer').innerHTML = `
+        <div class='registerContainer'>
+            Login: <br><input type='text' class='inpLogin' id='login2' name='inpLogin' required><br>
+            Hasło:<br><input type='password' class='inpPassword' id='password' required><br>
+            Potwierdź hasło:<br><input type='password' class='inpPassword' id='password2' required><br>
+            <button class='sendButton1'>Zarejestruj się</button>
+        </div>
+    `
+
+	const sendButton1 = document.querySelector('.sendButton1')
+	sendButton1.addEventListener('click', async () => {
+		const login = document.getElementById('login2').value
+		const password = document.getElementById('password').value
+		const password2 = document.getElementById('password2').value
+
+		if (password !== password2) {
+			alert('Hasła się nie zgadzają')
+			return
+		}
+
+		try {
+			const response = await fetch('http://localhost:3002/register', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({ login, password }),
+			})
+
+			if (response.ok) {
+				alert('Zarejestrowano pomyślnie!')
+				window.location.href = 'http://localhost:5173/'
+			} else {
+				const data = await response.json()
+				alert(data.message || 'Wystąpił błąd podczas rejestracji')
+			}
+		} catch (error) {
+			console.error('Wystąpił błąd:', error)
+			alert('Wystąpił błąd podczas rejestracji')
+		}
+	})
+})
--- logowanieNode/index.js
+++ logowanieNode/index.js
@@ -1,13 +1,55 @@
-const express = require("express");
-const app = express();
-const port = 3001;
-const cors = require("cors");
-const bodyparser = require("body-parser");
-const mysql = require("mysql");
+const express = require('express')
+const mysql = require('mysql')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
-app.use(bodyparser.json());
-app.use(cors());
-app.use(bodyparser.urlencoded({ extended: true }));
-app.use(express.json());
+const app = express()
+const port = 3002
 
-app.listen(3001, () => console.log("Sluchamy na porcie 3001!"));
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const db = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'projektnode',
+})
+
+db.connect(err => {
+	if (err) throw err
+	console.log('Połączono z bazą danych!')
+})
+
+app.post('/register', (req, res) => {
+	const { login, password } = req.body
+
+	db.query('SELECT * FROM users WHERE login = ?', [login], (err, results) => {
+		if (err) return res.status(500).json({ error: err })
+		if (results.length > 0) {
+			return res.status(400).json({ message: 'Użytkownik już istnieje' })
+		}
+
+		db.query('INSERT INTO users (login, password) VALUES (?, ?)', [login, password], err => {
+			if (err) return res.status(500).json({ error: err })
+			res.status(201).json({ message: 'Użytkownik zarejestrowany' })
+		})
+	})
+})
+
+app.post('/login', (req, res) => {
+	const { login, password } = req.body
+
+	db.query('SELECT * FROM users WHERE login = ? AND password = ?', [login, password], (err, results) => {
+		if (err) return res.status(500).json({ error: err })
+		if (results.length === 0) {
+			return res.status(401).json({ message: 'Nieprawidłowy login lub hasło' })
+		}
+		res.status(200).json({ message: 'Zalogowano pomyślnie' })
+	})
+})
+
+app.listen(port, () => {
+	console.log(`Dziala sb na porcie ${port}`)
+})
