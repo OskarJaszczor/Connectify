b55595ae33f65c67b4fb0301afb1200f75d0bc9a
spokoxd <134709586+spokoxd@users.noreply.github.com>
2024-11-05 10:17:24 +0100

    asdasd
    adasd

--- projekt/src/components/NewMessage.jsx
+++ projekt/src/components/NewMessage.jsx
@@ -1,60 +1,65 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 export default function NewMessage({ activeChannel }) {
-	const [message, newMessage] = useState('')
+  const [message, newMessage] = useState("");
 
-	const handleInputChange = event => {
-		newMessage(event.target.value)
-	}
+  const handleInputChange = (event) => {
+    newMessage(event.target.value);
+  };
 
-	const handleSendMessage = async () => {
-		if (message.trim() === '') return
-		console.log('Wiadomość do wysłania:', message)
-		try {
-			console.log('Wiadomość do wysłania juz dalej:', message)
+  const handleSendMessage = async () => {
+    if (message.trim() === "") return;
+    console.log("Wiadomość do wysłania:", message);
+    try {
+      console.log("Wiadomość do wysłania juz dalej:", message);
+      console.log(document.querySelector(".chat"));
+      document
+        .querySelector(".chat")
+        .scrollTo(0, document.querySelector(".chat").clientHeight + 400);
 
-			const response = await fetch('http://localhost:3000/messages', {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify({
-					content: message,
-					author: 'User1',
-					channel: activeChannel,
-					date: new Date().toLocaleDateString(),
-					hour: new Date().toLocaleTimeString(),
-				}),
-			})
+      const response = await fetch("http://localhost:3000/messages", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          content: message,
+          author: "User1",
+          channel: activeChannel,
+          date: new Date().toLocaleDateString(),
+          hour: new Date().toLocaleTimeString(),
+        }),
+      });
 
-			const result = await response.json()
-			if (result.success) {
-				console.log('Wiadomość wysłana. Czyszczenie pola.')
-				newMessage('')
-			} else {
-				newMessage('')
-			}
-		} catch (error) {
-			console.log('Nie udało się wysłać wiadomości!', error)
-		}
-	}
+      const result = await response.json();
+      if (result.success) {
+        console.log("Wiadomość wysłana. Czyszczenie pola.");
+        newMessage("");
+      } else {
+        newMessage("");
+      }
+    } catch (error) {
+      console.log("Nie udało się wysłać wiadomości!", error);
+    }
+  };
 
-	return (
-		<div className="newMessageContainer">
-			<input
-				type="text"
-				value={message}
-				onChange={handleInputChange}
-				placeholder="Type here..."
-				className="newMessageInput"
-			/>
-			<button
-				onClick={handleSendMessage}
-				type="submit"
-				style={{ backgroundImage: `url(./img/send.png)`, color: `red` }}
-				className="newMessageSend">
-				submit
-			</button>
-		</div>
-	)
+  return (
+    <div className="newMessageContainer">
+      <input
+        type="text"
+        value={message}
+        onChange={handleInputChange}
+        placeholder="Type here..."
+        className="newMessageInput"
+      />
+      <button
+        onClick={handleSendMessage}
+        type="submit"
+        style={{ backgroundImage: `url(./img/send.png)`, color: `red` }}
+        className="newMessageSend"
+      >
+        submit
+      </button>
+    </div>
+  );
 }
